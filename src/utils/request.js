4d030b5ab9bfa14df5deaefc879784c1f8c19d1b
zzt <2387083001@qq.com>
2019-05-24 09:52:34 +0800

    vue-admin-v1.0

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,8 +5,10 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  withCredentials: true, // send cookies when cross-domain requests
+  // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  // baseURL: 'http://94.191.93.90:53801', // url = base url + request url
+  baseURL: 'http://localhost:53801',
+  withCredentials: false, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
 // request interceptor
@@ -53,9 +55,9 @@ service.interceptors.response.use(
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
         // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
+        MessageBox.confirm('您已经在其他地方登录,您可以取消继续呆在这个页面, 或者重新登录', '提交退出', {
+          confirmButtonText: '重新登录',
+          cancelButtonText: '取消',
           type: 'warning'
         }).then(() => {
           store.dispatch('user/resetToken').then(() => {
@@ -65,7 +67,6 @@ service.interceptors.response.use(
       }
       return Promise.reject(res.message || 'error')
     } else {
-      console(res)
       return res
     }
   },
