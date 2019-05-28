84fcf381b6b050615034598209c94c3421c99c30
zzt <2387083001@qq.com>
2019-05-28 20:00:06 +0800

    vue-admin-v1.0

--- src/views/UserRecharge/index.vue
+++ src/views/UserRecharge/index.vue
@@ -1,103 +1,114 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <el-input v-model="listQuery.ip" placeholder="服务器IP" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
-      <el-date-picker
-        v-model="listQuery.bTime"
-        type="date"
-        style="vertical-align: top"
-        placeholder="开始时间"
-      />
-      <el-date-picker
-        v-model="listQuery.eTime"
-        type="date"
-        style="vertical-align: top"
-        placeholder="结束时间"
-      />
-      <span
-        style="margin: 0 10px;vertical-align: middle"
-      >最少金额:</span>
-      <el-input-number
-        v-model="listQuery.minMoney"
-        style="vertical-align: top"
-        label=""
-        @change="handleChange"
-      />
-      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
-        查询
-      </el-button>
-      <el-button v-waves :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download" @click="handleDownload">
-        下载表格
-      </el-button>
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <el-input v-model="listQuery.ip" placeholder="服务器IP" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+          <el-date-picker
+            v-model="listQuery.bTime"
+            type="date"
+            style="vertical-align: top"
+            placeholder="开始时间"
+          />
+          <el-date-picker
+            v-model="listQuery.eTime"
+            type="date"
+            style="vertical-align: top"
+            placeholder="结束时间"
+          />
+          <span
+            style="margin: 0 10px;vertical-align: middle"
+          >最少金额:</span>
+          <el-input-number
+            v-model="listQuery.minMoney"
+            style="vertical-align: top"
+            label=""
+            @change="handleChange"
+          />
+          <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+            查询
+          </el-button>
+          <el-button v-waves :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download" @click="handleDownload">
+            下载表格
+          </el-button>
+        </div>
+        <div>
+          <el-table
+            :key="tableKey"
+            v-loading="listLoading"
+            :data="list"
+            border
+            fit
+            highlight-current-row
+            style="width: 100%;"
+            @sort-change="sortChange"
+          >
+            <el-table-column label="所在服务器IP" align="center">
+              <template slot-scope="scope">
+                <span>{{ scope.row.IPAddress }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="所在服务器名称" align="center">
+              <template slot-scope="scope">
+                <span>{{ scope.row.GameName }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="用户游戏ID" align="center">
+              <template slot-scope="scope">
+                <span>{{ scope.row.UserID }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="手机号" align="center">
+              <template slot-scope="{row}">
+                <span>{{ row.PhoneNumber }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="在线充值总额" align="center" prop="TotalRechargeLimit" sortable="custom">
+              <template slot-scope="scope">
+                <span>{{ scope.row.TotalRechargeLimit }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="提现总额" prop="TotalWithdrawalLimit" sortable="custom">
+              <template slot-scope="scope">
+                <span>{{ scope.row.TotalWithdrawalLimit }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="注册时间" align="center" prop="RegisterTime" sortable="custom">
+              <template slot-scope="{row}">
+                <span>{{ row.RegisterTime | DateFormat | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="最后登陆时间" class-name="status-col" prop="LastLogonTime" sortable="custom">
+              <template slot-scope="{row}">
+                <span>{{ row.LastLogonTime | DateFormat | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="注册地址" class-name="status-col">
+              <template slot-scope="{row}">
+                <span>{{ row.RegisterIP }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="支付宝" class-name="status-col">
+              <template slot-scope="{row}">
+                <span>{{ row.AlipayNumber }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="银行卡" class-name="status-col">
+              <template slot-scope="{row}">
+                <span>{{ row.BankNumber }}</span>
+              </template>
+            </el-table-column>
+          </el-table>
+          <pagination
+            v-show="total>0"
+            :total="total"
+            :page.sync="listQuery.page"
+            :limit.sync="listQuery.limit"
+            @pagination="getList"
+          />
+        </div>
+      </el-card>
     </div>
-
-    <el-table
-      :key="tableKey"
-      v-loading="listLoading"
-      :data="list"
-      border
-      fit
-      highlight-current-row
-      style="width: 100%;"
-      @sort-change="sortChange"
-    >
-      <el-table-column label="所在服务器IP" align="center">
-        <template slot-scope="scope">
-          <span>{{ scope.row.IPAddress }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="所在服务器名称" align="center">
-        <template slot-scope="scope">
-          <span>{{ scope.row.GameName }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="用户游戏ID" align="center">
-        <template slot-scope="scope">
-          <span>{{ scope.row.UserID }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="手机号" align="center">
-        <template slot-scope="{row}">
-          <span>{{ row.PhoneNumber }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="在线充值总额" align="center" prop="TotalRechargeLimit" sortable="custom">
-        <template slot-scope="scope">
-          <span>{{ scope.row.TotalRechargeLimit }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="提现总额" prop="TotalWithdrawalLimit" sortable="custom">
-        <template slot-scope="scope">
-          <span>{{ scope.row.TotalWithdrawalLimit }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="注册时间" align="center" prop="RegisterTime" sortable="custom">
-        <template slot-scope="{row}">
-          <span>{{ row.RegisterTime | DateFormat | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="最后登陆时间" class-name="status-col" prop="LastLogonTime" sortable="custom">
-        <template slot-scope="{row}">
-          <span>{{ row.LastLogonTime | DateFormat | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="注册地址" class-name="status-col">
-        <template slot-scope="{row}">
-          <span>{{ row.RegisterIP }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="支付宝" class-name="status-col">
-        <template slot-scope="{row}">
-          <span>{{ row.AlipayNumber }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="银行卡" class-name="status-col">
-        <template slot-scope="{row}">
-          <span>{{ row.BankNumber }}</span>
-        </template>
-      </el-table-column>
-    </el-table>
-    <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
   </div>
 </template>
 
